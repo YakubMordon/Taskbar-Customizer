74da4828688651deb807dac4bf31d44992da6815
Jacob Mordon <YakubKazimiezh.Mordon.KN.2021@edu.lpnu.ua>
2024-06-11 09:25:29 +0300

    Deleted unused function in TaskbarAlignmentHelper class.

--- Taskbar_Customizer.Helpers/Helpers/Taskbar/TaskbarAlignmentHelper.cs
+++ Taskbar_Customizer.Helpers/Helpers/Taskbar/TaskbarAlignmentHelper.cs
@@ -30,23 +30,4 @@ public static class TaskbarAlignmentHelper
             key.SetValue(RegistryValueName, value, RegistryValueKind.DWord);
         }
     }
-
-    /// <summary>
-    /// Method for getting taskbar alignment.
-    /// </summary>
-    /// <returns>Indicator whether taskbar alignment is center.</returns>
-    /// <exception cref="NullReferenceException">Exception thrown when key is not found.</exception>
-    public static bool GetTaskbarAlignment()
-    {
-        using var key = Registry.CurrentUser.OpenSubKey(RegistryKeyPath, false);
-
-        if (key is not null)
-        {
-            var value = (int?)key.GetValue(RegistryValueName);
-
-            return value == 0;
-        }
-
-        throw new NullReferenceException();
-    }
 }
